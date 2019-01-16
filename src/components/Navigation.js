c2d27cd82e6be255b1436476020f98e33294c2d4
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-01-16 16:10:17 -0500

    ADD LINKS TO NAV COMPONENTS

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 const Navigation = () => {
   return (
     <div>
       <div className="App">
-        <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
-        </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
+          <ul>
+            <li>
+              <Link to={'/'}>Home</Link>
+            </li>
+            <li>
+              <Link to={'/about'}>About</Link>
+            </li>
+            <li>
+              <Link to={'/contact'}>Contact</Link>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
